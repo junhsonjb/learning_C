efd8ba670d79488afd9e16f765176272be13025b
Junhson <jjeanba2@binghamton.edu>
2017-12-18 21:25:06 -0500

    made the hello world

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#define JUNE "Junhson"
+
+int main() {
+	
+	printf("\n");
+	printf("hello world! My name is %s and it's very nice to meet you!", JUNE);
+	printf("\n");
+
+	return 0;
+}
